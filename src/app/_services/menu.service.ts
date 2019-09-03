9cd8c75949fd948ea69f5f183608acf3f6ed436e
nisala prasenajith rajapaksha <nisalarajapaksa@hotmail.com>
2019-09-03 12:09:11 +0530

    Menu component is added

---
+++ src/app/_services/menu.service.ts
@@ -0,0 +1,43 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { Menu } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class MenuService {
+    private currentMenuSubject: BehaviorSubject<Menu>;
+    public currentMenu: Observable<Menu>;
+
+    constructor(private http: HttpClient) {
+        this.currentMenuSubject = new BehaviorSubject<Menu>(JSON.parse(localStorage.getItem('currentMenu')));
+        this.currentMenu = this.currentMenuSubject.asObservable();
+    }
+
+    public get currentMenuValue(): Menu {
+        return this.currentMenuSubject.value;
+    }
+
+    GetMenu(userID: string, key: string, companyID: string) {
+        console.log("BEFOR...");
+        return this.http.post<any>(`http://35.200.254.194/PIC.V2/Services/PIC.Services/PIController.svc/BMGetUserMenuModules2`, 
+        { userID, key, companyID })
+            .pipe(map(menu => {
+                console.log("POST IN...");
+                if (menu) {
+                    console.log("Menu IN...");
+                    localStorage.setItem('currentMenu', JSON.stringify(menu));
+                    this.currentMenuSubject.next(menu);
+                }
+
+                return menu;
+            }));
+    }
+
+    ClearMenu() {
+        localStorage.removeItem('currentMenu');
+        this.currentMenuSubject.next(null);
+    }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 import { User } from '@app/_models';
-import { UserService, AuthenticationService } from '@app/_services';
+import { UserService, AuthenticationService, MenuService } from '@app/_services';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit, OnDestroy {
@@ -13,7 +13,8 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     constructor(
         private authenticationService: AuthenticationService,
-        private userService: UserService
+        private userService: UserService,
+        private menuService: MenuService
     ) {
         this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
             this.currentUser = user;
@@ -22,6 +23,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.loadAllUsers();
+        this.menuService.GetMenu("1", "QJ", "1");
     }
 
     ngOnDestroy() {
