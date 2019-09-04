52b480be1ab7675fe571b9c23aa8ac95eb5daaac
esshthuyiya <thusitha@brainweber.com>
2019-09-04 18:10:03 +0530

    Done

--- src/app/dynamic-page/dynamic-page.component.ts
+++ src/app/dynamic-page/dynamic-page.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { MenuService } from '@app/_services';
 
 import { User, Menu } from '@app/_models';
 
 @Component({ templateUrl: 'dynamic-page.component.html' })
-export class DynamicPageComponent implements OnInit, OnChanges {
+export class DynamicPageComponent implements OnInit {
     currentUser: User;
     currentUserSubscription: Subscription;
     menu: Menu;
@@ -19,19 +19,20 @@ export class DynamicPageComponent implements OnInit, OnChanges {
     ) {
         this.route.queryParams.subscribe(params => {
             this.moduleId = params.moduleId
+            this.setPageData()
         });
     }
 
     ngOnInit() {
+        
+    }
+
+    setPageData() {
         this.menu = this.menuService.currentMenuValue as Menu;
         this.menu['responseObjectMod'].map(item => {
             if (item.ModuleId == this.moduleId) {
                 this.pageTitle = item.ModuleName
             }
         })
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-        console.log(changes)
     }
 }
