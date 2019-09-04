54143604ab1b7d0abaec50a50396fbb6b7eb0d5e
esshthuyiya <thusitha@brainweber.com>
2019-09-04 16:58:28 +0530

    update top nav bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { ProfileComponent } from './profile';
+import { DynamicPageComponent } from './dynamic-page';
 import { AuthGuard } from './_guards';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+    { path: 'dynamic', component: DynamicPageComponent, canActivate: [AuthGuard] },
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
